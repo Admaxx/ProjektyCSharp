79aa650f834c66293498ed996786b68b4a133f56
admaxx <JakubSroka96@gmail.com>
2021-02-03 00:27:18 +0100

    Nothing Special

--- Webówka/WPFTraining/WebPages.xaml.cs
+++ Webówka/WPFTraining/WebPages.xaml.cs
@@ -22,51 +22,79 @@ namespace WPFTraining
         public WebPages()
         {
             InitializeComponent();
+            WebPage.Source = new Uri("https://google.com");
+            BackButton.IsEnabled = false;
+            ForwardButton.IsEnabled = false;
+
         }
 
         private void WebSearch_Click(object sender, RoutedEventArgs e)
         {
-
+           
             try
             {
-
+                BackButton.IsEnabled = true;
                 WebPage.Source = new Uri(UrlGrabber.Text);
-
+                //ActualAddress();
+               
             }
             catch (Exception)
             {
+                
+               
+                
                 WebPage.Source = new Uri("http://www."+UrlGrabber.Text);
                 
                 UrlGrabber.Text = "http://www." + UrlGrabber.Text;
+                //ActualAddress();
             }
         }
 
         private void Back_Click(object sender, RoutedEventArgs e)
         {
-            try
+
+            WebPage.GoBack();
+            if (WebPage.CanGoBack == false)
             {
+                BackButton.IsEnabled = false;
+
+
+            }
+            else
+            {
+                ForwardButton.IsEnabled = true;
+                MessageBox.Show("Fajki");
+                BackButton.IsEnabled = true;
                 WebPage.GoBack();
             }
-            catch (System.Runtime.InteropServices.COMException)
-            {
-
-                
-            }
-
+                    
+                    //ActualAddress();
         }
 
         private void Forward_Click(object sender, RoutedEventArgs e)
         {
-            try
+            WebPage.GoForward();
+            if(WebPage.CanGoForward == false)
             {
-                WebPage.GoForward();
-            }
-            catch (System.Runtime.InteropServices.COMException)
-            {
+                ForwardButton.IsEnabled = false;
 
-                
             }
+            else
+            {
+                ForwardButton.IsEnabled = true;
+                BackButton.IsEnabled = true;
+                WebPage.GoForward();
+
+            }
+               
+                //ActualAddress();
+
             
+        }
+        private void ActualAddress()
+        {
+            UrlGrabber.Text = Convert.ToString(WebPage.Source);
+
         }
     }
 }
