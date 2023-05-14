8abbe6c6a4f79054abcca5a8af9e54f7701d53d0
Kuba <JakubSroka@gmail.com>
2023-05-15 01:27:02 +0200

    Adding unit test - one for now.

---
+++ PaperStore/PaperStoreUnitTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using PaperStore.Services.Delete;
+using PaperStore.WareHouseData;
+
+namespace PaperStoreUnitTests
+{
+    [TestClass]
+    public class DeleteTests
+    {
+        [TestMethod]
+        public void SendingZeroIdToDelete_ShouldReturnTask()
+        {
+            PaperWarehouseContext context = new();
+
+            RemoveItem r = new RemoveItem(context);
+
+            Assert.AreNotEqual(r.Item(0), "Usuniêto poprawnie"); //It should return a task
+
+
+        }
+    }
+}
