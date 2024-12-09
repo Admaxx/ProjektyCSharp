8965504bc3305662098a781ad89d857a42f441ff
Admaxx <56221663+Admaxx@users.noreply.github.com>
2024-12-09 16:51:41 +0100

    Register page added 1/2 ver. 0.0.0.1

---
+++ JustTalk/JustTalk/Controllers/InfoController.cs
@@ -0,0 +1,18 @@
+﻿using JustTalk.justTalkModels;
+using JustTalk.Services.Account;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JustTalk.Controllers
+{
+    public class InfoController : Controller
+    {
+        InsertToDBNew db;
+        public IActionResult SignUpSucc(User user)
+        {
+            db = new InsertToDBNew(user);
+
+
+            return db.insertNewUser() ? View() : RedirectToAction("SignUp", "Account", new { alreadyExistsUserMess = "Jest juz ktos taki, downie" });
+        }
+    }
+}
--- JustTalk/JustTalk/obj/Debug/net8.0/JustTalk.AssemblyInfo.cs
+++ JustTalk/JustTalk/obj/Debug/net8.0/JustTalk.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JustTalk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f10b5d2408a62b2954bfcae71741cc3addc4cd73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce53af656edf32f8a6e8e37dfe262cdb693ec4a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("JustTalk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JustTalk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
